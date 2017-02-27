075988212bb47eb4864e02ee8d6fc2d402124ad5
gaolitong <gao_litong1984@hotmail.com>
2017-02-27 16:55:35 +0800

    2nd commit

--- Knapsack.py
+++ Knapsack.py
@@ -58,8 +58,35 @@ def testGreedys(foods, maxUnits):
     print('\nUse greedy by density to allocate', maxUnits, 'calories')
     testGreedy(foods, maxUnits, Food.density)    
 
+def maxValue(foods, maxUnits):
+    if foods == [] or maxUnits == 0:
+        result = (0, ())
+    elif foods[0].getCost() > maxUnits:
+        result = maxValue(foods[1:], maxUnits)
+    else:
+        nextItem = foods[0]
+        #select left side
+        withValue, withToTake = maxValue(foods[1:], 
+                                         maxUnits - nextItem.getCost())
+        withValue += nextItem.getValue()
+        #select right side
+        withOutValue, withOutTake = maxValue(foods[1:], maxUnits)
+        #compare which way is best
+        if withValue > withOutValue:
+            result = (withValue,withToTake + (nextItem,))
+        else:
+            result = (withOutValue, withOutTake)
+    return result
+def testMaxValue(foods, maxUnits):
+    print('\ntest the maxValue method')
+    print(maxValue(foods, maxUnits))
+    val , taken = maxValue(foods, maxUnits)
+    for item in taken:
+        print(' ', item)
+    
 names = ['wine', 'beer', 'pizza', 'burger', 'fries', 'coke', 'apple', 'donut']
 values = [89,90,95,100,90,79,50,10]
 calories = [123,154,258,354,365,150,95,195]
 foods = buildMenu(names, values, calories)
-testGreedys(foods, 900)
+#testGreedys(foods, 95)
+testMaxValue(foods, 95)
